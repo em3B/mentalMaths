3fe4c6f23e598141af3c237e8b5ada816109168a
elord <emilym.baird@gmail.com>
2025-03-06 18:54:48 +0000

    add model changes

--- db/migrate/20250304132435_create_topics.rb
+++ db/migrate/20250304132435_create_topics.rb
@@ -1,10 +1,11 @@
 class CreateTopics < ActiveRecord::Migration[8.0]
   def change
     create_table :topics do |t|
-      t.string :title
+      t.string :title, null: false, unique: true
       t.text :intro
 
       t.timestamps
     end
+    add_index :topics, :title, unique: true
   end
 end
