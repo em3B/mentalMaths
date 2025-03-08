6c635f09e6f450f39354406839b0a87ef29f91ff
elord <emilym.baird@gmail.com>
2025-03-08 20:31:01 +0000

    Style Landing Page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link application.scss
+//= link application.css
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// app/javascript/packs/application.js
+import 'bootstrap';
