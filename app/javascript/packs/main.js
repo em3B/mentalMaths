818f02059815ed430137f1ba5b3cd27e7b5130e1
elord <emilym.baird@gmail.com>
2025-04-20 09:07:38 +0100

    Enable Devise Signout

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // app/javascript/packs/main.js
 import 'bootstrap';
+import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs";
+
+Rails.start();
