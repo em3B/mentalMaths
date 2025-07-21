a9c2421ac40f42af95469a0d01ce692445cb81c0
elord <emilym.baird@gmail.com>
2025-07-21 20:43:34 +0100

    Add Custom Error Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get "errors/not_found"
+  get "errors/internal_server_error"
+  get "errors/unprocessable_entity"
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unprocessable_entity', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
+  if Rails.env.development?
+    get '/test_404', to: 'errors#not_found'
+    get '/test_422', to: 'errors#unprocessable_entity'
+    get '/test_500', to: 'errors#internal_server_error'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
