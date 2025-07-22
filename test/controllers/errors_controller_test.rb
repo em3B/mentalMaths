5c14c32c27135bd83d5a65cc9653e5f7266dbac2
elord <emilym.baird@gmail.com>
2025-07-22 18:19:59 +0100

    update unit test

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -3,16 +3,16 @@ require "test_helper"
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get not_found" do
     get errors_not_found_url
-    assert_response :success
+    assert_response :not_found  # 404
   end
 
   test "should get internal_server_error" do
     get errors_internal_server_error_url
-    assert_response :success
+    assert_response :internal_server_error  # 500
   end
 
   test "should get unprocessable_entity" do
     get errors_unprocessable_entity_url
-    assert_response :success
+    assert_response :unprocessable_entity  # 422
   end
 end
