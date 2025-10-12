66594b3283e54e9e9256a75eeb8655d834944568
elord <emilym.baird@gmail.com>
2025-10-12 17:52:46 +0100

    add unit  tests for new controller

--- test/controllers/capacity_requests_controller_test.rb
+++ test/controllers/capacity_requests_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 class CapacityRequestsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers  # make Devise helpers available
+
+  def setup
+    @user = users(:one)   # replace with a valid fixture
+    sign_in @user
+  end
+
   test "should get new" do
-    get capacity_requests_new_url
+    get new_capacity_request_url
     assert_response :success
   end
 
-  test "should get create" do
-    get capacity_requests_create_url
-    assert_response :success
+  test "should create capacity_request" do
+    assert_difference("CapacityRequest.count") do
+      post capacity_requests_url, params: { capacity_request: { name: "Test" } }
+    end
+    assert_redirected_to capacity_request_url(CapacityRequest.last)
   end
 end
