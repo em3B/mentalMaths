8c25c56302bed01feba0dc32bbe627583b7112ad
elord <emilym.baird@gmail.com>
2025-12-01 17:49:24 +0000

    update test

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
-class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
+class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)  # or create a user with FactoryBot if you prefer
+    sign_in @user
+  end
+
   test "should get teacher dashboard" do
     get teacher_dashboard_url
     assert_response :success
