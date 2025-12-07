bd0a0232ef9ddb8f47035cfa6f0627ce104d6054
elord <emilym.baird@gmail.com>
2025-12-07 16:33:53 +0000

    update profile controller unit test

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,18 +1,34 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(
+      email: "teacher@example.com",
+      password: "password123",
+      role: "teacher",
+      username: "test"
+    )
+
+    sign_in @user
+  end
+
   test "should get show" do
-    get profiles_show_url
+    get profile_path   # <- changed
     assert_response :success
   end
 
   test "should get edit" do
-    get profiles_edit_url
+    get edit_profile_path  # <- changed
     assert_response :success
   end
 
-  test "should get update" do
-    get profiles_update_url
-    assert_response :success
+  test "should update profile" do
+    patch profile_path, params: { user: { name: "New Name", email: "new@example.com" } }  # <- changed
+    assert_redirected_to profile_path
+    @user.reload
+    assert_equal "New Name", @user.name
+    assert_equal "new@example.com", @user.email
   end
 end
